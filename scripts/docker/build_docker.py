e4b2d7ce2bd3021256f7c3a607ae185ab8c9ece8
VJalili <VJalili@users.noreply.github.com>
2022-12-19 10:37:56 -0500

    test

--- scripts/docker/build_docker.py
+++ scripts/docker/build_docker.py
@@ -150,7 +150,9 @@ class ProjectBuilder:
             launch_script_path: str = Paths.this_script_folder
     ):
         if project_arguments.docker_repo is not None:
+            print("------------------ trying to login")
             os.system("docker login")
+            print("------------------ after login")
 
         self.project_arguments = project_arguments
         self.launch_script_path = launch_script_path
