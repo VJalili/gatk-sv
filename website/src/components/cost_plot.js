3339b7f1b5b21c06c8c6ba6af81c8e043266e96c
vjalili <VJalili@users.noreply.github.com>
2023-10-20 14:18:22 -0400

    test

--- website/src/components/cost_plot.js
+++ website/src/components/cost_plot.js
@@ -1,24 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Plot from 'react-plotly.js';
+import { useColorMode } from '@docusaurus/theme-common';
 
-class CostPlot extends React.Component {
-  render() {
-    return (
-      <Plot
-        data={[
-          {
-            x: [1, 2, 3],
-            y: [2, 6, 3],
-            type: 'scatter',
-            mode: 'lines+markers',
-            marker: {color: 'red'},
-          },
-          {type: 'bar', x: [1, 2, 3], y: [2, 5, 3]},
-        ]}
-        layout={ {title: 'A Fancy Plot'} }
-      />
-    );
-  }
+function CostPlot() {
+  const { colorMode, setColorMode } = useColorMode();
+
+  return (
+    <Plot
+      layout={{ title: 'A Fancy Plot', autosize: true }}
+      useResizeHandler={true}
+      style={{ width: "100%", height: "100%" }}
+      data={[
+        {
+          y: [1, 2, 3, 4, 4, 4, 8, 9, 10],
+          type: 'box',
+          name: 'Sample A',
+          marker: {
+            color: colorMode == 'dark' ? 'rgb(214,12,140)' : 'rgb(0,0,0)',
+          }
+        },
+        {
+          y: [2, 3, 3, 3, 3, 5, 6, 6, 7],
+          type: 'box',
+          name: 'Sample B',
+          marker: {
+            color: 'rgb(0,128,128)'
+          }
+        },
+      ]}
+    />
+  );
 }
 
-export default CostPlot
+export default CostPlot;
